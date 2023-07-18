a9e1d819ebe26e55729c2fc4fe7319a30c275aab
sonukuldeep <sonukuldeep@outlook.com>
2023-07-18 14:19:10 +0530

    particle system 4

--- particle system/script.js
+++ particle system/script.js
@@ -13,18 +13,40 @@ ctx.strokeStyle = 'white';
 var fps = 30; // Frames per second
 var interval = 1000 / fps; // Interval between frames in milliseconds
 var lastTime = 0;
+var requestAnimationFrameRef = 0;
+var mouse = {
+    x: -1,
+    y: -1,
+    radius: 100
+};
 window.addEventListener('resize', function () {
+    cancelAnimationFrame(requestAnimationFrameRef);
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
+    effect = new Effect(canvas);
+    animation(0);
+});
+window.addEventListener('mousedown', function (e) {
+    var clientX = e.clientX, clientY = e.clientY;
+    mouse.x = clientX;
+    mouse.y = clientY;
+});
+window.addEventListener('mouseup', function () {
+    mouse.x = -1;
+    mouse.y = -1;
 });
 var Particle = /** @class */ (function () {
     function Particle(effect) {
         this.effect = effect;
-        this.radius = 5 + Math.random() * 20;
+        this.radius = Math.floor(5 + Math.random() * 10);
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
         this.vx = ((Math.random() * 20) - 10) / this.radius;
         this.vy = ((Math.random() * 20) - 10) / this.radius;
+        this.force = 60 / (this.radius * this.radius);
+        this.pushX = 0;
+        this.pushY = 0;
+        this.friction = 0.98;
         this.fillColorFactor = 360 / canvas.width;
     }
     Particle.prototype.draw = function (context) {
@@ -32,15 +54,34 @@ var Particle = /** @class */ (function () {
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
-        // context.stroke()
     };
     Particle.prototype.update = function () {
-        this.x += this.vx;
-        this.y += this.vy;
-        if ((this.x + this.radius) > this.effect.width || (this.x - this.radius) < 0)
+        if (mouse.x != -1 && mouse.y != -1) {
+            var dx = this.x - mouse.x;
+            var dy = this.y - mouse.y;
+            var distance = Math.hypot(dx, dy);
+            if (distance < mouse.radius) {
+                var angle = Math.atan2(dy, dx);
+                this.pushX += Math.cos(angle) * this.force;
+                this.pushY += Math.sin(angle) * this.force;
+            }
+        }
+        this.x += this.vx + this.pushX;
+        this.y += this.vy + this.pushY;
+        if ((this.x + this.radius) > this.effect.width || (this.x - this.radius) < 0) {
+            this.x = this.x;
             this.vx *= -1;
-        if ((this.y + this.radius) > this.effect.height || (this.y - this.radius) < 0)
+            this.pushX *= -1;
+        }
+        if ((this.y + this.radius) > this.effect.height || (this.y - this.radius) < 0) {
+            this.y = this.y;
             this.vy *= -1;
+            this.pushY *= -1;
+        }
+        this.pushX *= this.friction;
+        this.pushY *= this.friction;
+    };
+    Particle.prototype.handleMouseMove = function () {
     };
     return Particle;
 }());
@@ -63,6 +104,7 @@ var Effect = /** @class */ (function () {
             particle.draw(context);
             particle.update();
         });
+        // this.connectParticles(context)
     };
     Effect.prototype.connectParticles = function (context) {
         var maxDistance = 100;
@@ -93,7 +135,6 @@ function animation(timestamp) {
     // Proceed only if enough time has elapsed based on the desired frame rate
     if (elapsedTime > interval) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // effect.connectParticles(ctx)
         effect.handleParticles(ctx);
         // Update your animation or game logic here
         // Render your animation or game state here
@@ -101,6 +142,6 @@ function animation(timestamp) {
         lastTime = timestamp;
     }
     // Request the next frame
-    requestAnimationFrame(animation);
+    requestAnimationFrameRef = requestAnimationFrame(animation);
 }
 animation(0);
--- particle system/script.ts
+++ particle system/script.ts
@@ -10,14 +10,34 @@ canvas.height = document.documentElement.clientHeight
 // gradient.addColorStop(1, 'violet')
 // ctx.fillStyle = gradient
 ctx.strokeStyle = 'white'
-var fps = 30; // Frames per second
-var interval = 1000 / fps; // Interval between frames in milliseconds
-var lastTime = 0;
+const fps = 30; // Frames per second
+const interval = 1000 / fps; // Interval between frames in milliseconds
+let lastTime = 0;
+let requestAnimationFrameRef = 0
 
+const mouse = {
+    x: -1,
+    y: -1,
+    radius: 100
+}
 
 window.addEventListener('resize', () => {
+    cancelAnimationFrame(requestAnimationFrameRef)
     canvas.width = document.documentElement.clientWidth
     canvas.height = document.documentElement.clientHeight
+    effect = new Effect(canvas)
+    animation(0)
+})
+
+window.addEventListener('mousedown', (e) => {
+    const { clientX, clientY } = e
+    mouse.x = clientX
+    mouse.y = clientY
+})
+
+window.addEventListener('mouseup', () => {
+    mouse.x = -1
+    mouse.y = -1
 })
 
 class Particle {
@@ -27,15 +47,23 @@ class Particle {
     effect: Effect;
     vx: number;
     vy: number;
+    force: number;
+    pushX: number;
+    pushY: number;
+    friction: number;
     fillColorFactor: number;
 
     constructor(effect: Effect) {
         this.effect = effect
-        this.radius = 5 + Math.random() * 20
+        this.radius = Math.floor(5 + Math.random() * 10)
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2)
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2)
         this.vx = ((Math.random() * 20) - 10) / this.radius
         this.vy = ((Math.random() * 20) - 10) / this.radius
+        this.force = 60 / (this.radius * this.radius)
+        this.pushX = 0
+        this.pushY = 0
+        this.friction = 0.98
         this.fillColorFactor = 360 / canvas.width
     }
 
@@ -44,13 +72,38 @@ class Particle {
         context.beginPath()
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
         context.fill()
-        // context.stroke()
     }
     update() {
-        this.x += this.vx
-        this.y += this.vy
-        if ((this.x + this.radius) > this.effect.width || (this.x - this.radius) < 0) this.vx *= -1
-        if ((this.y + this.radius) > this.effect.height || (this.y - this.radius) < 0) this.vy *= -1
+        if (mouse.x != -1 && mouse.y != -1) {
+            const dx = this.x - mouse.x
+            const dy = this.y - mouse.y
+            const distance = Math.hypot(dx, dy)
+            if (distance < mouse.radius) {
+                const angle = Math.atan2(dy, dx)
+                this.pushX += Math.cos(angle) * this.force
+                this.pushY += Math.sin(angle) * this.force
+            }
+        }
+
+        this.x += this.vx + this.pushX
+        this.y += this.vy + this.pushY
+
+        if ((this.x + this.radius) > this.effect.width || (this.x - this.radius) < 0) {
+            this.x = this.x
+            this.vx *= -1
+            this.pushX *= -1
+        }
+        if ((this.y + this.radius) > this.effect.height || (this.y - this.radius) < 0) {
+            this.y = this.y
+            this.vy *= -1
+            this.pushY *= -1
+        }
+
+        this.pushX *= this.friction
+        this.pushY *= this.friction
+    }
+    handleMouseMove() {
+
     }
 }
 
@@ -81,6 +134,7 @@ class Effect {
             particle.draw(context)
             particle.update()
         })
+        // this.connectParticles(context)
     }
     connectParticles(context: CanvasRenderingContext2D) {
         const maxDistance = 100
@@ -91,7 +145,7 @@ class Effect {
                 const distance = Math.hypot(dx, dy)
                 if (distance < maxDistance) {
                     context.save()
-                    const opacity = 1 - distance/maxDistance
+                    const opacity = 1 - distance / maxDistance
                     context.globalAlpha = opacity
                     context.beginPath()
                     context.moveTo(this.particles[a].x, this.particles[a].y)
@@ -105,7 +159,7 @@ class Effect {
     }
 }
 
-const effect = new Effect(canvas)
+let effect = new Effect(canvas)
 
 function animation(timestamp) {
     // Calculate the time difference since the last frame
@@ -114,18 +168,18 @@ function animation(timestamp) {
     // Proceed only if enough time has elapsed based on the desired frame rate
     if (elapsedTime > interval) {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        // effect.connectParticles(ctx)
         effect.handleParticles(ctx)
         // Update your animation or game logic here
 
         // Render your animation or game state here
 
         // Update the last time to the current timestamp
+
         lastTime = timestamp;
     }
 
     // Request the next frame
-    requestAnimationFrame(animation);
+    requestAnimationFrameRef = requestAnimationFrame(animation);
 }
 
 animation(0)
