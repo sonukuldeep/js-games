ba6312bb123345990352b84895d7a4fd67e62489
sonukuldeep <sonukuldeep@outlook.com>
2023-07-18 10:46:47 +0530

    particle system 3

--- particle system/script.js
+++ particle system/script.js
@@ -2,11 +2,13 @@ var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
-var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient.addColorStop(0, 'white');
-gradient.addColorStop(0.5, 'magenta');
-gradient.addColorStop(1, 'blue');
-ctx.fillStyle = gradient;
+// const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+// gradient.addColorStop(0, 'red')
+// gradient.addColorStop(0.4, 'blue')
+// gradient.addColorStop(0.5, 'green')
+// gradient.addColorStop(0.6, 'yellow')
+// gradient.addColorStop(1, 'violet')
+// ctx.fillStyle = gradient
 ctx.strokeStyle = 'white';
 var fps = 30; // Frames per second
 var interval = 1000 / fps; // Interval between frames in milliseconds
@@ -23,12 +25,14 @@ var Particle = /** @class */ (function () {
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
         this.vx = ((Math.random() * 20) - 10) / this.radius;
         this.vy = ((Math.random() * 20) - 10) / this.radius;
+        this.fillColorFactor = 360 / canvas.width;
     }
     Particle.prototype.draw = function (context) {
+        context.fillStyle = "hsl(".concat(this.x * this.fillColorFactor, ", 100%, 50%)");
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
-        context.stroke();
+        // context.stroke()
     };
     Particle.prototype.update = function () {
         this.x += this.vx;
@@ -60,6 +64,26 @@ var Effect = /** @class */ (function () {
             particle.update();
         });
     };
+    Effect.prototype.connectParticles = function (context) {
+        var maxDistance = 100;
+        for (var a = 0; a < this.particles.length; a++) {
+            for (var b = a; b < this.particles.length; b++) {
+                var dx = this.particles[a].x - this.particles[b].x;
+                var dy = this.particles[a].y - this.particles[b].y;
+                var distance = Math.hypot(dx, dy);
+                if (distance < maxDistance) {
+                    context.save();
+                    var opacity = 1 - distance / maxDistance;
+                    context.globalAlpha = opacity;
+                    context.beginPath();
+                    context.moveTo(this.particles[a].x, this.particles[a].y);
+                    context.lineTo(this.particles[b].x, this.particles[b].y);
+                    context.stroke();
+                    context.restore();
+                }
+            }
+        }
+    };
     return Effect;
 }());
 var effect = new Effect(canvas);
@@ -69,6 +93,7 @@ function animation(timestamp) {
     // Proceed only if enough time has elapsed based on the desired frame rate
     if (elapsedTime > interval) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // effect.connectParticles(ctx)
         effect.handleParticles(ctx);
         // Update your animation or game logic here
         // Render your animation or game state here
--- particle system/script.ts
+++ particle system/script.ts
@@ -2,11 +2,13 @@ const canvas = document.querySelector('canvas')!
 const ctx = canvas.getContext('2d')!
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
-const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
-gradient.addColorStop(0, 'white')
-gradient.addColorStop(0.5, 'magenta')
-gradient.addColorStop(1, 'blue')
-ctx.fillStyle = gradient
+// const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+// gradient.addColorStop(0, 'red')
+// gradient.addColorStop(0.4, 'blue')
+// gradient.addColorStop(0.5, 'green')
+// gradient.addColorStop(0.6, 'yellow')
+// gradient.addColorStop(1, 'violet')
+// ctx.fillStyle = gradient
 ctx.strokeStyle = 'white'
 var fps = 30; // Frames per second
 var interval = 1000 / fps; // Interval between frames in milliseconds
@@ -25,6 +27,7 @@ class Particle {
     effect: Effect;
     vx: number;
     vy: number;
+    fillColorFactor: number;
 
     constructor(effect: Effect) {
         this.effect = effect
@@ -33,13 +36,15 @@ class Particle {
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2)
         this.vx = ((Math.random() * 20) - 10) / this.radius
         this.vy = ((Math.random() * 20) - 10) / this.radius
+        this.fillColorFactor = 360 / canvas.width
     }
 
     draw(context: CanvasRenderingContext2D) {
+        context.fillStyle = `hsl(${this.x * this.fillColorFactor}, 100%, 50%)`
         context.beginPath()
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
         context.fill()
-        context.stroke()
+        // context.stroke()
     }
     update() {
         this.x += this.vx
@@ -77,6 +82,27 @@ class Effect {
             particle.update()
         })
     }
+    connectParticles(context: CanvasRenderingContext2D) {
+        const maxDistance = 100
+        for (let a = 0; a < this.particles.length; a++) {
+            for (let b = a; b < this.particles.length; b++) {
+                const dx = this.particles[a].x - this.particles[b].x
+                const dy = this.particles[a].y - this.particles[b].y
+                const distance = Math.hypot(dx, dy)
+                if (distance < maxDistance) {
+                    context.save()
+                    const opacity = 1 - distance/maxDistance
+                    context.globalAlpha = opacity
+                    context.beginPath()
+                    context.moveTo(this.particles[a].x, this.particles[a].y)
+                    context.lineTo(this.particles[b].x, this.particles[b].y)
+                    context.stroke()
+                    context.restore()
+                }
+            }
+
+        }
+    }
 }
 
 const effect = new Effect(canvas)
@@ -88,6 +114,7 @@ function animation(timestamp) {
     // Proceed only if enough time has elapsed based on the desired frame rate
     if (elapsedTime > interval) {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
+        // effect.connectParticles(ctx)
         effect.handleParticles(ctx)
         // Update your animation or game logic here
 
